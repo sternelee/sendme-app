8080d0364cf9ccc74ba85e75f7c9c3c9b4b368f6
sternelee <sternelee@gmail.com>
2026-01-07 12:13:55 +0800

    feat: upgrade vue version

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,14 +1,21 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import VueJsxVapor from "vue-jsx-vapor/vite";
 import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 
-// @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vite.dev/config/
 export default defineConfig(async () => ({
-  plugins: [vue(), tailwindcss()],
+  plugins: [
+    vue(),
+    VueJsxVapor({
+      macros: true,
+      interop: true,
+    }),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
